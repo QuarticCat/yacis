907ca240b0bebac9358263917b8786bb356c0be1
QuarticCat <QuarticCat@protonmail.com>
2020-04-23 18:20:11 +0800

    Add utility and update main

---
+++ include/yacis/utility/print_tree.hpp
@@ -0,0 +1,54 @@
+#ifndef YACIS_PRINT_TREE_HPP_
+#define YACIS_PRINT_TREE_HPP_
+
+namespace yacis::utility {
+
+/**
+ * @brief Convert given node tag to its name in c style string.
+ */
+inline constexpr const char* tag_to_str(ast::NodeTag tag) {
+    switch (tag) {
+    case ast::NodeTag::kRoot:
+        return "Root";
+    case ast::NodeTag::kIntLit:
+        return "IntLit";
+    case ast::NodeTag::kBoolLit:
+        return "BoolLit";
+    case ast::NodeTag::kCharLit:
+        return "CharLit";
+    case ast::NodeTag::kVarName:
+        return "VarName";
+    case ast::NodeTag::kTypeName:
+        return "TypeName";
+    case ast::NodeTag::kType:
+        return "Type";
+    case ast::NodeTag::kApplExpr:
+        return "ApplExpr";
+    case ast::NodeTag::kCondExpr:
+        return "CondExpr";
+    case ast::NodeTag::kLetExpr:
+        return "LetExpr";
+    case ast::NodeTag::kLambdaParam:
+        return "LambdaParam";
+    case ast::NodeTag::kLambdaExpr:
+        return "LambdaExpr";
+    case ast::NodeTag::kTypeAlias:
+        return "TypeAlias";
+    case ast::NodeTag::kTypeAssign:
+        return "TypeAssign";
+    case ast::NodeTag::kValueAssign:
+        return "ValueAssign";
+    case ast::NodeTag::kOutput:
+        return "Output";
+    }
+}
+
+inline void print_tree(std::unique_ptr<ast::BaseNode>& root, int indent = 0) {
+    for (int i = 0; i < indent; ++i) std::cout.put(' ');
+    std::cout << "- " << tag_to_str(root->tag) << std::endl;
+    for (auto&& i : root->children) print_tree(i, indent + 2);
+}
+
+}  // namespace yacis::utility
+
+#endif  // YACIS_PRINT_TREE_HPP_
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <memory>
+#include <string>
 
+#include "tao/pegtl/contrib/parse_tree.hpp"
+#include "yacis/analysis/check.hpp"
+#include "yacis/ast/node.hpp"
+#include "yacis/ast/selector.hpp"
+#include "yacis/utility/print_tree.hpp"
+
+// Temporary main
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    auto in = tao::pegtl::file_input("../examples/pair.yac");
+    auto root = tao::pegtl::parse_tree::parse<yacis::grammar::Grammar,
+                                              yacis::ast::BaseNode,
+                                              yacis::ast::Selector>(in);
+    yacis::utility::print_tree(root);
+    yacis::analysis::check(root);
 }
