c762b339261ea35b30d6b94bd1d05a192a7b8413
QuarticCat <QuarticCat@protonmail.com>
2020-05-27 04:39:39 +0800

    Add CLI support

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
+#include <fstream>
 #include <iostream>
 
 #include "yacis/yacis.hpp"
 
-// Temporary main
-int main() {
-    std::cout << yacis::compile_to_asm("../examples/primality_test.yac");
+int main(int argc, char* argv[]) {
+    if (argc == 2)
+        std::cout << yacis::compile_to_asm(argv[1]);
+    else if (argc == 3)
+        std::ofstream(argv[2]) << yacis::compile_to_asm(argv[1]);
+    else
+        std::cout << "Unknown argument number.";
 }
