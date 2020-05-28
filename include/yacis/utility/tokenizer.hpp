0286588a189744dae0ea21dbfe20037fe224971a
QuarticCat <QuarticCat@protonmail.com>
2020-05-29 05:23:43 +0800

    Update tokenizer.hpp

--- include/yacis/utility/tokenizer.hpp
+++ include/yacis/utility/tokenizer.hpp
@@ -1,6 +1,7 @@
 #ifndef YACIS_UTILITY_TOKENIZER_HPP_
 #define YACIS_UTILITY_TOKENIZER_HPP_
 
+#include <algorithm>
 #include <cctype>
 #include <cstring>
 #include <string>
@@ -12,7 +13,9 @@ enum class TokenTag {
     kIntLit,
     kCharLit,
     kBoolLit,
-    kIdent,
+    kUCID,
+    kLCID,
+    kKeyword,
     kEmpty,
     kComment,
     kSymbol
@@ -29,41 +32,43 @@ inline std::vector<Token> tokenize(const std::string& input) {
     const char* begin = input.data();
     const char* end = input.data() + input.size();
     for (const char* i = begin; i < end;) {
-        if (isdigit(*i)) {
-            const char* tmp = i++;
+        const char* tmp = i;
+        if (std::isdigit(*i)) {
+            i += 1;
             while (i < end && isdigit(*i)) ++i;
             ret.push_back({tmp, i, TokenTag::kIntLit});
-        } else if (*i == '-' && i + 1 < end && isdigit(*(i + 1))) {
-            const char* tmp = i;
+        } else if (*i == '-' && i + 1 < end && std::isdigit(*(i + 1))) {
             i += 2;
             while (i < end && isdigit(*i)) ++i;
             ret.push_back({tmp, i, TokenTag::kIntLit});
         } else if (*i == '\'') {
-            const char* tmp = i++;
+            i += 1;
             while (i < end && (*i != '\'' || *(i - 1) == '\\')) ++i;
             ret.push_back({tmp, i, TokenTag::kCharLit});
-        } else if (*i == 'T' && !strcmp(i + 1, "rue")) {
-            const char* tmp = i;
+        } else if (std::equal(i, i + 4, "True")) {
             ret.push_back({tmp, i += 4, TokenTag::kBoolLit});
-        } else if (*i == 'F' && !strcmp(i + 1, "alse")) {
-            const char* tmp = i;
+        } else if (std::equal(i, i + 5, "False")) {
             ret.push_back({tmp, i += 5, TokenTag::kBoolLit});
         } else if (isalpha(*i) || *i == '_') {
-            const char* tmp = i++;
-            while (i < end && (isalpha(*i) || *i == '_')) ++i;
-            ret.push_back({tmp, i, TokenTag::kIdent});
+            i += 1;
+            while (i < end && (isalpha(*i) || isdigit(*i) || *i == '_')) ++i;
+            if (isupper(*tmp))
+                ret.push_back({tmp, i, TokenTag::kUCID});
+            else if (std::equal(tmp, i, "if") || std::equal(tmp, i, "then") ||
+                     std::equal(tmp, i, "else") || std::equal(tmp, i, "data"))
+                ret.push_back({tmp, i, TokenTag::kKeyword});
+            else
+                ret.push_back({tmp, i, TokenTag::kLCID});
         } else if (isspace(*i)) {
-            const char* tmp = i++;
+            i += 1;
             while (i < end && isspace(*i)) ++i;
             ret.push_back({tmp, i, TokenTag::kEmpty});
         } else if (*i == '-' && i + 1 < end && *(i + 1) == '-') {
-            const char* tmp = i;
             i += 2;
             while (i < end && *i != '\n') ++i;
             ret.push_back({tmp, i, TokenTag::kComment});
         } else {
-            const char* tmp = i++;
-            ret.push_back({tmp, i, TokenTag::kSymbol});
+            ret.push_back({tmp, ++i, TokenTag::kSymbol});
         }
     }
     return ret;
