ebe7eb34ce95322099aa7280b1dfa3f08eae9f39
QuarticCat <QuarticCat@protonmail.com>
2020-05-29 08:31:47 +0800

    Update error

--- include/yacis/analysis/error.hpp
+++ include/yacis/analysis/error.hpp
@@ -10,32 +10,13 @@
 
 namespace yacis::analysis {
 
-enum class ErrorType { kTypeError, kDefineError };
-
-/**
- * @brief Convert given error type to its name in c style string.
- */
-inline constexpr const char* error_type_to_cstr(ErrorType error_type) {
-    switch (error_type) {
-    case ErrorType::kTypeError:
-        return "TypeError";
-    case ErrorType::kDefineError:
-        return "DefineError";
-    default:
-        return "UnknownError";
-    }
-}
-
 class CompileError: public std::exception {
   public:
     using pos_t = tao::pegtl::internal::iterator;
 
-    CompileError(ErrorType type,
-                 pos_t pos,
-                 const std::string& error_message) noexcept:
+    CompileError(pos_t pos, const std::string& error_message) noexcept:
         message(std::to_string(pos.line) + ":" +
-                std::to_string(pos.byte_in_line) + " - " +
-                error_type_to_cstr(type) + ": " + error_message) {}
+                std::to_string(pos.byte_in_line) + " - " + error_message) {}
 
     CompileError(const CompileError& other) noexcept: message(other.message) {}
 
@@ -55,13 +36,13 @@ class CompileError: public std::exception {
 class TypeError: public CompileError {
   public:
     TypeError(pos_t pos, const std::string& error_message) noexcept:
-        CompileError(ErrorType::kTypeError, pos, error_message) {}
+        CompileError(pos, "TypeError: " + error_message) {}
 };
 
 class DefineError: public CompileError {
   public:
     DefineError(pos_t pos, const std::string& error_message) noexcept:
-        CompileError(ErrorType::kDefineError, pos, error_message) {}
+        CompileError(pos, "DefineError: " + error_message) {}
 };
 
 }  // namespace yacis::analysis
