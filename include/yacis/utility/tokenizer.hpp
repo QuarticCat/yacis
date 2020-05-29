10f010890bea08867f4c6c77a0200ecc0e0e35d1
QuarticCat <QuarticCat@protonmail.com>
2020-05-30 01:05:09 +0800

    Update tokenizer.hpp

--- include/yacis/utility/tokenizer.hpp
+++ include/yacis/utility/tokenizer.hpp
@@ -35,11 +35,11 @@ inline std::vector<Token> tokenize(const std::string& input) {
         const char* tmp = i;
         if (std::isdigit(*i)) {
             i += 1;
-            while (i < end && isdigit(*i)) ++i;
+            while (i < end && std::isdigit(*i)) ++i;
             ret.push_back({tmp, i, TokenTag::kIntLit});
         } else if (*i == '-' && i + 1 < end && std::isdigit(*(i + 1))) {
             i += 2;
-            while (i < end && isdigit(*i)) ++i;
+            while (i < end && std::isdigit(*i)) ++i;
             ret.push_back({tmp, i, TokenTag::kIntLit});
         } else if (*i == '\'') {
             i += 1;
@@ -49,19 +49,19 @@ inline std::vector<Token> tokenize(const std::string& input) {
             ret.push_back({tmp, i += 4, TokenTag::kBoolLit});
         } else if (std::equal(i, i + 5, "False")) {
             ret.push_back({tmp, i += 5, TokenTag::kBoolLit});
-        } else if (isalpha(*i) || *i == '_') {
+        } else if (std::isalpha(*i) || *i == '_') {
             i += 1;
-            while (i < end && (isalpha(*i) || isdigit(*i) || *i == '_')) ++i;
-            if (isupper(*tmp))
+            while (i < end && (std::isalnum(*i) || *i == '_')) ++i;
+            if (std::isupper(*tmp))
                 ret.push_back({tmp, i, TokenTag::kUCID});
             else if (std::equal(tmp, i, "if") || std::equal(tmp, i, "then") ||
                      std::equal(tmp, i, "else") || std::equal(tmp, i, "data"))
                 ret.push_back({tmp, i, TokenTag::kKeyword});
             else
                 ret.push_back({tmp, i, TokenTag::kLCID});
-        } else if (isspace(*i)) {
+        } else if (std::isspace(*i)) {
             i += 1;
-            while (i < end && isspace(*i)) ++i;
+            while (i < end && std::isspace(*i)) ++i;
             ret.push_back({tmp, i, TokenTag::kEmpty});
         } else if (*i == '-' && i + 1 < end && *(i + 1) == '-') {
             i += 2;
