a3aaddb9a3c757718e28836f4239f12cbbe7d2bf
QuarticCat <QuarticCat@protonmail.com>
2020-05-30 05:34:19 +0800

    Change output

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,15 @@
 #include "yacis/yacis.hpp"
 
 int main(int argc, char* argv[]) {
-    if (argc == 2)
-        std::cout << yacis::compile_to_asm(argv[1]);
-    else if (argc == 3)
-        std::ofstream(argv[2]) << yacis::compile_to_asm(argv[1]);
-    else
-        std::cout << "Unknown argument number.";
+    try {
+        if (argc == 2)
+            std::cout << yacis::compile_to_asm(argv[1]) << std::endl;
+        else if (argc == 3)
+            std::ofstream(argv[2])
+                << yacis::compile_to_asm(argv[1]) << std::endl;
+        else
+            std::cerr << "Unknown argument number." << std::endl;
+    } catch (const yacis::analysis::CompileError& e) {
+        std::cerr << e.what() << std::endl;
+    }
 }
